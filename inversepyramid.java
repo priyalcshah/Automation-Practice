7e681df2daa036299397981cf65c4b0121485742
priyal <priyalcshah@gmail.com>
2024-03-27 21:04:23 +0530

    Initial commit

---
+++ inversepyramid.java
@@ -0,0 +1,23 @@
+
+public class inversepyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print inverse pyramid
+		int p=1;
+		for(int i=0;i<4;i++)
+		{
+			//System.out.println("Outer loop started");
+			for(int j=1;j<=i+1;j++)
+			{
+				System.out.print(p);
+				System.out.print("\t");
+				p++;
+			}
+			//System.out.println("Outer loop finished");
+			System.out.println("");
+			
+		}
+	}
+
+}
